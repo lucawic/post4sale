451e5cc3693905831d2d65f2af486e7956db9add
lucawic <lucaswick@email.arizona.edu>
2021-11-17 21:07:38 -0700

    connection to mongoose not working :/

--- server/index.js
+++ server/index.js
@@ -1,4 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://lucawic:Nero-Reach@cluster0.8cfih.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+mongoose.set('useFinedandModify', false);
+
+// https://www.mongodb.com/cloud/atlas
