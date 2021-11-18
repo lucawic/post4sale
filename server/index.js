a60e7ec148b1842dbfeb0fe8fe5be4e2c110d108
Kevin Phan <kevin.b.phan97@gmail.com>
2021-11-18 03:26:35 -0700

    Auth front-end and back-end might delete google auth

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 dotenv.config();
@@ -13,8 +14,9 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
-// const CONNECTION_URL = 'mongodb+srv://lucawic:Nero-Reach@cluster0.8cfih.mongodb.net/post4sale?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://lucawic:Nero-Reach@cluster0.8cfih.mongodb.net/post4sale?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 // Connection to mongodb
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,14 +1,13 @@
 import express from 'express';
-
-import { getPosts, getPost, createPost, updatePost, likePost,deletePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, likePost,deletePost } from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
-router.get('/', getPost);
-router.get('/', createPost);
-router.patch('/:id', updatePost)
-router.delete('/:id', deletePost)
-router.patch('/:id/likePost', likePost)
+router.get('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 export default router;
