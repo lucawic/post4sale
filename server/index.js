a36e06a51ff57376e7a839763f52c53fec3f68a6
lucawic <lucaswick@email.arizona.edu>
2021-11-17 21:18:21 -0700

    database connected :)

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors())
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://lucawic:Nero-Reach@cluster0.8cfih.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://lucawic:Nero-Reach@cluster0.8cfih.mongodb.net/post4sale?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-.catch((error) => console.log(error.message));
+  .then(() => app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFinedandModify', false);
-
-// https://www.mongodb.com/cloud/atlas
+mongoose.set('useFindAndModify', false);
